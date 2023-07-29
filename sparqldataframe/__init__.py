e9698e172733df6f04f34e8b87f7fb2c19207b93
Nikolai Janakiev <nikolai.janakiev@gmail.com>
2023-07-29 14:04:42 +0200

    Add common prefixes

--- sparqldataframe/__init__.py
+++ sparqldataframe/__init__.py
@@ -1,18 +1,39 @@
-__version__ = '0.1.1'
+__version__ = '0.1.2'
 
 import pandas as pd
 from simplejson import JSONDecodeError
 import requests
 
 
+DEFAULT_PREFIXES = """
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX owl: <http://www.w3.org/2002/07/owl#>
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>
+
+PREFIX dbo: <http://dbpedia.org/ontology/>
+PREFIX dbr: <http://dbpedia.org/resource/>
+PREFIX dbp: <http://dbpedia.org/property/>
+
+PREFIX wd: <http://www.wikidata.org/entity/>
+PREFIX wdt: <http://www.wikidata.org/prop/direct/>
+PREFIX wikibase: <http://wikiba.se/ontology#>
+PREFIX p: <http://www.wikidata.org/prop/>
+PREFIX ps: <http://www.wikidata.org/prop/statement/>
+PREFIX pq: <http://www.wikidata.org/prop/qualifier/>
+PREFIX bd: <http://www.bigdata.com/rdf#>
+"""
+
+
 def dbpedia_query(sparql_query):
     url = 'http://dbpedia.org/sparql'
-    return query(url, sparql_query)
+    return query(url, DEFAULT_PREFIXES + sparql_query)
 
 
 def wikidata_query(sparql_query):
     url = 'https://query.wikidata.org/sparql'
-    return query(url, sparql_query)
+    return query(url, DEFAULT_PREFIXES + sparql_query)
 
 
 def query(url, sparql_query):
